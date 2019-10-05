f64dd65619c723c1e4c7139168ef4ec013f7b4bc
WhiteLie1 <1136462446@qq.com>
2019-10-05 20:30:56 +0800

    10.5java基础语法

---
+++ javademo/java基础/Demo01Const.java
@@ -0,0 +1,80 @@
+/*public class Demo01Const{
+	public static void main(String[] args){
+		//字符串常量
+		System.out.println("ABC");
+		System.out.println("");//字符串两引号中间的内容为空
+		System.out.println("xyz");
+	}
+}*/
+
+//变量的示例
+/*public class Demo01Const{
+	public static void main(String[] args){
+		int num1;
+		num1 = 10;
+		System.out.println(num1);
+		//改变变量当中本来数字，变成新的数字
+		num1 = 20;
+		System.out.println(num1);
+		System.out.println("***************");
+
+		byte num2 = 127;
+		System.out.println(num2);
+	}
+}*/
+
+/*public class Demo01Const{
+	public static void main(String[] args){
+		int num1 = 10;
+
+	}
+}*/
+
+/*public class Demo01Const{
+	public static void main(String[] args){
+		System.out.println(1024);
+	}
+}*/
+
+/*public class Demo01Const{
+	public static void main(String[] args){
+		int num = (int)100L;
+		System.out.println(num);
+	}
+}*/
+
+/*public class Demo01Const{
+	public static void main(String[] args){
+		char zifu1 = '1';
+		System.out.println(zifu1+0);//49
+	}
+}*/
+
+public class Demo01Const{
+	public static void main(String[] args){
+		//int a = 20;
+		//int b = 30;
+		//System.out.println(a-b);//49
+		int x = 10;
+		int y = 3;
+		int result1 = x/y;
+		System.out.println(result1);
+		
+		int result2 = x % y;
+		System.out.println(result2);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
