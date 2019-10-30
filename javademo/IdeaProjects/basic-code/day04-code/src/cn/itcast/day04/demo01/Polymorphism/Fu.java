ba300899249f65034e58350c025481aec87f92cd
WhiteLie1 <1136462446@qq.com>
2019-10-30 20:54:10 +0800

    多态

---
+++ javademo/IdeaProjects/basic-code/day04-code/src/cn/itcast/day04/demo01/Polymorphism/Fu.java
@@ -0,0 +1,11 @@
+package cn.itcast.day04.demo01.Polymorphism;
+
+public class Fu {
+    int num = 10;
+    public void method(){
+        System.out.println("父类方法！");
+    }
+    public void methodFu(){
+        System.out.println("父类特有的方法");
+    }
+}
---
+++ javademo/IdeaProjects/basic-code/day04-code/src/cn/itcast/day04/demo01/Polymorphism/Zi.java
@@ -0,0 +1,9 @@
+package cn.itcast.day04.demo01.Polymorphism;
+
+public class Zi extends Fu {
+    int num = 20;
+    @Override
+    public void method() {
+        System.out.println("子类方法");
+    }
+}
