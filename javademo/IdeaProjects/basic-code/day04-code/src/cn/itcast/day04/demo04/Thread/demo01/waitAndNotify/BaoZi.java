c1ce73ffb6b396e756dccb2d352e6730cce80dac
WhiteLie1 <1136462446@qq.com>
2019-12-30 21:03:07 +0800

    线程通信

---
+++ javademo/IdeaProjects/basic-code/day04-code/src/cn/itcast/day04/demo04/Thread/demo01/waitAndNotify/BaoZi.java
@@ -0,0 +1,18 @@
+package cn.itcast.day04.demo04.Thread.demo01.waitAndNotify;
+/*
+    资源类：包子类
+    设置包子的属性
+        皮
+        馅
+        包子的状态：有true,没用false
+
+ */
+public class BaoZi {
+    // 皮
+    String pi;
+    // 陷
+    String xian;
+    // 包子的状态：有true,没false,设置初始值为false没有包子
+    boolean flag = false;
+
+}
